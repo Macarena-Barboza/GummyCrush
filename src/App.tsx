39e49bfbe6a66007a6f4cb23d19df76a0101f2a4
Macarena Barboza <macarenabarbozatri@hotmail.com>
2024-06-18 23:56:46 -0300

    Finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { useCallback, useEffect, useState } from "react";
+import { DragEvent, useCallback, useEffect, useState } from "react";
 import "./App.css";
 import { COLORS, WIDTH } from "./constants";
 
 function App() {
     const [candies, setCandies] = useState<string[]>([]);
+    const [dragging, setDragging] = useState<number>(-1);
+    const [dropping, setDropping] = useState<number>(-1);
 
     const createGrid = () => {
         const colors: string[] = new Array(WIDTH * WIDTH)
@@ -36,11 +38,13 @@ function App() {
 
             if (
                 currentCandies[i] === currentCandies[i + WIDTH] &&
-                currentCandies[i] === currentCandies[i + WIDTH * 2]
+                currentCandies[i] === currentCandies[i + WIDTH * 2] &&
+                currentCandies[i] === currentCandies[i + WIDTH * 3]
             ) {
                 currentCandies[i] = "";
                 currentCandies[i + WIDTH] = "";
                 currentCandies[i + WIDTH * 2] = "";
+                currentCandies[i + WIDTH * 3] = "";
             }
         }
         return currentCandies;
@@ -121,13 +125,52 @@ function App() {
             nextCandies = moveDown(nextCandies);
 
             if (
-                currentCandies.every((candies, i) => candies === nextCandies[i])
+                currentCandies.every((candis, i) => candis === nextCandies[i])
             ) {
                 return nextCandies;
             }
             currentCandies = [...nextCandies];
         }
     }, []);
+
+    const preventDefaultBehavior = (e: DragEvent<HTMLDivElement>) =>
+        e.preventDefault();
+
+    const dragStart = (e: DragEvent<HTMLDivElement>) => {
+        setDragging(Number(e.currentTarget.getAttribute("data-id")));
+    };
+    const dragdrop = (e: DragEvent<HTMLDivElement>) => {
+        setDropping(Number(e.currentTarget.getAttribute("data-id")));
+    };
+    const dragEnd = () => {
+        const isNextCell =
+            dropping === dragging - 1 ||
+            dropping === dragging + 1 ||
+            dropping === dragging - WIDTH ||
+            dropping === dragging + WIDTH;
+
+        const invalidRightEdge =
+            dragging % WIDTH === WIDTH - 1 && dropping === dragging + 1;
+        const invalidLeftEdge =
+            dragging % WIDTH === 0 && dropping === dragging - 1;
+        const valid = isNextCell && !invalidLeftEdge && !invalidRightEdge;
+
+        if (valid) {
+            let newCandies = [...candies];
+            [newCandies[dragging], newCandies[dropping]] = [
+                newCandies[dropping],
+                newCandies[dragging],
+            ];
+            newCandies = checkColum(newCandies);
+            newCandies = checkRow(newCandies);
+
+            if (newCandies.filter((x) => !x).length === 0) {
+                return;
+            } else {
+                setCandies(newCandies);
+            }
+        }
+    };
 
     useEffect(() => {
         let newCandies = createGrid();
@@ -137,15 +180,38 @@ function App() {
         setCandies(newCandies);
     }, [removeChains]);
 
+    useEffect(() => {
+        setTimeout(() => {
+            let newCandies = [...candies];
+            newCandies = checkColum(newCandies);
+            newCandies = checkRow(newCandies);
+            newCandies = moveDown(newCandies);
+            if (newCandies.every((candy, index) => candy === candies[index])) {
+                return;
+            } else {
+                setCandies(newCandies);
+            }
+        }, 100);
+    }, [candies]);
+
     return (
         <div className="app">
             <h1>Gummy Crush</h1>
 
             <div className="board">
                 {candies.map((candy, index) => (
-                    <div className={candy} key={index}>
-                        {index}
-                    </div>
+                    <div
+                        key={index}
+                        className={candy}
+                        data-id={index}
+                        draggable
+                        onDragStart={dragStart}
+                        onDragOver={preventDefaultBehavior}
+                        onDragEnter={preventDefaultBehavior}
+                        onDragLeave={preventDefaultBehavior}
+                        onDrop={dragdrop}
+                        onDragEnd={dragEnd}
+                    ></div>
                 ))}
             </div>
         </div>
